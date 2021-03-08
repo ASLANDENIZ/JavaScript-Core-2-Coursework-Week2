7fe2e917eb72f625dc30b713d24a344ed21160b7
Deniz Kelemence <dkelemence@gmail.com>
2021-03-08 10:21:17 +0000

    working on it!

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,24 @@
+
+
+For each object in the array:
+
+- Add a`<h1>` tag to the html containing the name of the person.
+- Add a`<h2>` tag to the html containing the job of the person.
+- All of your HTML should go inside the`<div>` with the id ** "content" **:
+
+```html
+<div id="content">
+  <h1>{Name Here}</h1>
+  <h2>{Job Here}</h2>
+  .....
+</div>
+```
+
+
+
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  let headingOne = document.createElement("h1");
 }
 
 let people = [
@@ -9,3 +28,5 @@ let people = [
 ];
 
 listOfNames(people);
+
+
