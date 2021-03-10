63beb8ac5d09748e9d64ec146271cd7576dc9b9b
Deniz Kelemence <dkelemence@gmail.com>
2021-03-10 00:37:08 +0000

    exercises are done!

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,35 @@
+// * You should use an`<ul>` for the list and`<li>` tags for the contents.
+// * Each todo should have an event listener for when they are clicked.
+// * The event listener should add the line - through style to the todo.
+// * The event listener should also remove the line - through style conditionally by checking if the style is already applied.
+// * All of your HTML should go inside the`<div>` with the id ** "content" **.
+
+
 function todoList(todos) {
+  let contentEl = document.getElementById("content");
+  let ulEl = document.createElement("ul");
+  contentEl.appendChild(ulEl);
+
+  for (let i = 0; i < todos.length; i++) {
+    let liEl = document.createElement("li");
+    liEl.innerHTML = todos[i].todo;
+    ulEl.appendChild(liEl);
+    liEl.addEventListener("click", addRemoveLine);
+    function addRemoveLine() {
+      liEl.classList.toggle("visible");
+    }
+   
+
+  }
+
   // Write your code here...
 }
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" }
 ];
 
 todoList(todos);
--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,6 +1,27 @@
+
+
+/
 function listOfColours(colours) {
-  // Write your code here...
+  let contentEl = document.getElementById("content");
+  // * Create a`<select>` element.
+  let selectEl = document.createElement("select");
+  contentEl.appendChild(selectEl);
+  // * Create a`<p>` element.
+  let pEl = document.createElement("p");
+  contentEl.appendChild(pEl);
+  // * Iterate through the array of colours.
+  for (let i = 0; i < colours.length; i++) {
+    let optionEl = document.createElement("option");
+    optionEl.innerHTML= colours[i];
+    selectEl.appendChild(optionEl);  
+  }
+  selectEl.addEventListener("click", function () {
+    pEl.innerHTML = `You have selected: ${selectEl.value}`;
+    pEl.style.color = selectEl.value;
+  }
+  )
 }
+
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
 
